d9cdb4c54dab735d43dc31a4ef8e6627b23a5416
AboudyKreidieh <akreidieh@gmail.com>
2018-10-17 00:53:38 -0700

    added travis components

---
+++ tests/fast_tests/test_models.py
@@ -0,0 +1,8 @@
+"""Contains tests for the model abstractions and different models."""
+import unittest
+
+
+class TestTravis(unittest.TestCase):
+    """A simple test to get Travis running."""
+
+    pass
