7d838f717c84978ba2a9343913beebde9f85bf18
AboudyKreidieh <akreidieh@gmail.com>
2018-11-19 15:50:33 -0800

    converted models to classes

--- hbaselines/models/fcnet.py
+++ hbaselines/models/fcnet.py
@@ -1,62 +1,79 @@
+from hbaselines.models.base import Model
 import tensorflow as tf
 
 
-def fully_connected_network(inputs,
-                            num_outputs,
-                            hidden_size,
-                            nonlinearity,
-                            stochastic,
-                            scope):
-    """Create a deep feedforward neural network model.
+class FullyConnectedNetwork(Model):
 
-    Parameters
-    ----------
-    inputs : tf.placeholder
-        input placeholder
-    num_outputs : int
-        number of outputs from the neural network
-    hidden_size : list of int
-        a list that specified the shape of the neural network
-    nonlinearity : tf.nn.*
-        activation nonlinearity
-    stochastic : bool
-        a boolean operator that specifies whether the policy is meant to be
-        stochastic or deterministic. If it is stochastic, an additional
-        trainable variable is created to compute the logstd
-    scope : str
-        scope of the model
+    def __init__(self,
+                 sess,
+                 inputs,
+                 num_outputs,
+                 hidden_size,
+                 nonlinearity,
+                 stochastic,
+                 scope):
+        """Create a deep feedforward neural network model.
 
-    Returns
-    -------
-    tf.Variable
-        output mean from the model
-    tf.Variable or None
-        output std from the model. If the model is not stochastic, then this
-        output is set to None.
-    """
-    with tf.variable_scope(scope):
-        # create the hidden layers
-        last_layer = inputs
-        for i, hidden in enumerate(hidden_size):
-            last_layer = tf.layers.dense(
+        Parameters
+        ----------
+        sess : tf.Session
+            tf session (shared between the model and algorithm
+        inputs : tf.placeholder
+            input placeholder
+        num_outputs : int
+            number of outputs from the neural network
+        hidden_size : list of int
+            a list that specified the shape of the neural network
+        nonlinearity : tf.nn.*
+            activation nonlinearity
+        stochastic : bool
+            a boolean operator that specifies whether the policy is meant to be
+            stochastic or deterministic. If it is stochastic, an additional
+            trainable variable is created to compute the logstd
+        scope : str
+            scope of the model
+
+        Returns
+        -------
+        tf.Variable
+            output mean from the model
+        tf.Variable or None
+            output std from the model. If the model is not stochastic, then
+            this output is set to None.
+        """
+        Model.__init__(self, sess, inputs)
+
+        # store information on whether the policy is stochastic
+        self.stochastic = stochastic
+
+        with tf.variable_scope(scope):
+            # create the hidden layers
+            last_layer = inputs
+            for i, hidden in enumerate(hidden_size):
+                last_layer = tf.layers.dense(
+                    inputs=last_layer,
+                    units=hidden,
+                    activation=nonlinearity)
+
+            # create the output layer
+            output_mean = tf.layers.dense(
                 inputs=last_layer,
-                units=hidden,
-                activation=nonlinearity)
+                units=num_outputs,
+                activation=None)
 
-        # create the output layer
-        output_mean = tf.layers.dense(
-            inputs=last_layer,
-            units=num_outputs,
-            activation=None)
+        if stochastic:
+            # Create a trainable variable whose output is the same size as
+            # the action space. This variable will represent the output log
+            # standard deviation of your stochastic policy.
+            output_logstd = tf.get_variable(name="action_logstd",
+                                            shape=[num_outputs],
+                                            trainable=True)
+        else:
+            output_logstd = None
 
-    if stochastic:
-        # Create a trainable variable whose output is the same size as the
-        # action space. This variable will represent the output log standard
-        # deviation of your stochastic policy.
-        output_logstd = tf.get_variable(name="action_logstd",
-                                        shape=[num_outputs],
-                                        trainable=True)
-    else:
-        output_logstd = None
+        self.output_ph = output_mean
+        self.output_logstd_ph = output_logstd
 
-    return output_mean, output_logstd
+    def get_action(self, state):
+        """See parent class."""
+        pass
--- hbaselines/models/linear.py
+++ hbaselines/models/linear.py
@@ -1,36 +1,44 @@
+from hbaselines.models.base import Model
 import tensorflow as tf
 
 
-def linear(inputs,
-           num_outputs,
-           scope,
-           weights_initializer=None,
-           bias_initializer=0):
-    """Create a linear model.
+class LinearModel(Model):
 
-    Parameters
-    ----------
-    inputs : tf.placeholder
-        input placeholder
-    num_outputs : int
-        number of outputs from the neural network
-    scope : str
-        scope of the model
-    weights_initializer : tf.Operation
-        initialization operation for the weights of the model
-    bias_initializer : tf.Operation
-        initialization operation for the biases of the model
+    def __init__(self,
+                 sess,
+                 inputs,
+                 num_outputs,
+                 scope,
+                 weights_initializer=None,
+                 bias_initializer=0):
+        """Create a linear model.
 
-    Returns
-    -------
-    tf.Variable
-        output from the model
-    """
-    with tf.variable_scope(scope):
-        w = tf.get_variable("W", [inputs.get_shape()[1], num_outputs],
-                            initializer=weights_initializer)
-        b = tf.get_variable("b", [num_outputs],
-                            initializer=tf.constant_initializer(
-                                bias_initializer))
+        Parameters
+        ----------
+        sess : tf.Session
+            tf session (shared between the model and algorithm
+        inputs : tf.placeholder
+            input placeholder
+        num_outputs : int
+            number of outputs from the neural network
+        scope : str
+            scope of the model
+        weights_initializer : tf.Operation
+            initialization operation for the weights of the model
+        bias_initializer : tf.Operation
+            initialization operation for the biases of the model
+        """
+        Model.__init__(self, sess, inputs)
 
-    return tf.matmul(inputs, w) + b
+        with tf.variable_scope(scope):
+            w = tf.get_variable("W", [inputs.get_shape()[1], num_outputs],
+                                initializer=weights_initializer)
+            b = tf.get_variable("b", [num_outputs],
+                                initializer=tf.constant_initializer(
+                                    bias_initializer))
+
+        self.output_ph = tf.matmul(inputs, w) + b
+
+    def get_action(self, state):
+        """See parent class."""
+        self.sess.run(self.output_ph, feed_dict={self.input_ph: state})
