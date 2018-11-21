88f4e239834052d2b69667fb8b54b822ad376e0d
AboudyKreidieh <akreidieh@gmail.com>
2018-11-21 14:57:55 -0800

    utility method to help in logging results

---
+++ hbaselines/utils/logger.py
@@ -0,0 +1,12 @@
+import os
+import errno
+
+
+def ensure_dir(path):
+    """Ensure that the directory specified exists, and if not, create it."""
+    try:
+        os.makedirs(path)
+    except OSError as exception:
+        if exception.errno != errno.EEXIST:
+            raise
+    return path
