70e69499a669a2925dbbf33fb4d851e7ddf1c563
Fangyu Wu <fywu85@gmail.com>
2018-11-29 18:22:02 -0800

    Add initial ray tuner scripts.

---
+++ experiments/mutiagent_hrl/lstm_baseline_ray.py
@@ -0,0 +1,20 @@
+"""A runner script for LSTM models.
+
+This run script used to test the performance of DDPG and DQN with LSTM models
+on various environments.
+
+Arguments
+---------
+env_name : str
+    name of the gym environment. This environment must either be registered in
+    gym, be available in the computation framework Flow, or be available within
+    the hbaselines/envs folder.
+alg : str
+    name of the algorithm to be implemented (the algorithm must be available
+    from hbaselines.algs)
+blank : blank  # TODO: add argument for hyperparameters
+    blank
+"""
+
+if __name__ == '__main__':
+    pass
