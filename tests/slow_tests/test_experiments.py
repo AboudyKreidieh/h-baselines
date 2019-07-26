eb83599f4278f7b38d8baa5d39f114110b467622
AboudyKreidieh <akreidieh@gmail.com>
2019-07-25 18:49:32 -0700

    tests for hiro_baseline

--- tests/slow_tests/test_experiments.py
+++ tests/slow_tests/test_experiments.py
@@ -2,8 +2,13 @@
 import unittest
 import os
 import shutil
+import ray
 
-from experiments.fcnet_baseline import main as fcnet_baselines
+from hbaselines.common.train import parse_options
+from experiments.fcnet_baseline import main as fcnet_baseline
+from experiments.hiro_baseline import main as hiro_baseline
+
+ray.init(num_cpus=1, redirect_output=True)
 
 
 class TestExperimentRunnerScripts(unittest.TestCase):
@@ -11,10 +16,9 @@ class TestExperimentRunnerScripts(unittest.TestCase):
 
     def test_fcent_baseline(self):
         # Run the script; verify it executes without failure.
-        fcnet_baselines(
-            args=["MountainCarContinuous-v0", "--n_cpus", "1",
-                  "--steps", "1000"]
-        )
+        args = parse_options('', '', args=["MountainCarContinuous-v0",
+                                           "--n_cpus", "1", "--steps", "1000"])
+        fcnet_baseline(args, 'data/fcnet')
 
         # Check that the folders were generated.
         self.assertTrue(os.path.isdir(
@@ -23,16 +27,20 @@ class TestExperimentRunnerScripts(unittest.TestCase):
         # Clear anything that was generated.
         shutil.rmtree(os.path.join(os.getcwd(), "data"))
 
-    # def test_hiro_baseline(self):
-    #     # Run the script; verify it executes without failure.
-    #     hiro_baseline(
-    #         args=["MountainCarContinuous-v0", "--n_cpus", "1",
-    #               "--steps", "1000"]
-    #     )
-    #
-    #     # Check that the folders were generated.
-    #
-    #     # Clear anything that was generated.
+    def test_hiro_baseline(self):
+        # Run the script; verify it executes without failure.
+        args = parse_options('', '', args=["MountainCarContinuous-v0",
+                                           "--n_cpus", "1", "--steps", "1000"])
+        hiro_baseline(args, 'data/goal-directed')
+
+        # Check that the folders were generated.
+        self.assertTrue(os.path.isdir(
+            os.path.join(
+                os.getcwd(),
+                "data/goal-directed/MountainCarContinuous-v0")))
+
+        # Clear anything that was generated.
+        shutil.rmtree(os.path.join(os.getcwd(), "data"))
 
 
 if __name__ == '__main__':
