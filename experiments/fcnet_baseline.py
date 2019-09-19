f4edef1e0cd194acbf42b56a81bc748bd428bd2d
AboudyKreidieh <akreidieh@gmail.com>
2019-09-19 14:55:53 -0700

    added seeds to runner scripts

--- experiments/fcnet_baseline.py
+++ experiments/fcnet_baseline.py
@@ -15,7 +15,7 @@ from hbaselines.hiro import TD3, FeedForwardPolicy
 EXAMPLE_USAGE = 'python fcnet_baseline.py "HalfCheetah-v2" --n_cpus 3'
 
 
-def run_exp(env, hp, steps, dir_name, evaluate, i):
+def run_exp(env, hp, steps, dir_name, evaluate, seed, i):
     """Run a single training procedure.
 
     Parameters
@@ -30,6 +30,8 @@ def run_exp(env, hp, steps, dir_name, evaluate, i):
         the location the results files are meant to be stored
     evaluate : bool
         whether to include an evaluation environment
+    seed : int
+        specified the random seed for numpy, tensorflow, and random
     i : int
         an increment term, used for logging purposes
     """
@@ -41,7 +43,7 @@ def run_exp(env, hp, steps, dir_name, evaluate, i):
         total_timesteps=steps,
         log_dir=dir_name,
         log_interval=10000,
-        seed=None,
+        seed=seed,
         exp_num=i
     )
 
@@ -65,7 +67,8 @@ def main(args, base_dir):
         w.writerow(hp)
 
     for i in range(args.n_training):
-        run_exp(args.env_name, hp, args.steps, dir_name, args.evaluate, i)
+        run_exp(args.env_name, hp, args.steps, dir_name, args.evaluate,
+                args.seed, i)
 
 
 if __name__ == '__main__':
