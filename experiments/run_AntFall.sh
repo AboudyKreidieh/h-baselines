dd9c584dc06b1326dd933dd205ad7bf553e7f3c7
AboudyKreidieh <akreidieh@gmail.com>
2019-12-03 17:49:54 -0800

    some cleanup

--- experiments/run_AntFall.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-for ((i=0;i<10;i+=1))
-do
-  # running goal-conditioned experiments without any augmentations
-	python run_hrl.py "AntFall" --evaluate --total_steps 10000000 --relative_goals --use_huber --seed $i
-done
