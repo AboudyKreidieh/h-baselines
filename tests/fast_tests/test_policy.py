be2b3df1158b20836771479e2a0bdddd134941f3
AboudyKreidieh <akreidieh@gmail.com>
2020-01-11 19:48:28 +0200

    added four tests

--- tests/fast_tests/test_policy.py
+++ tests/fast_tests/test_policy.py
@@ -50,7 +50,7 @@ class TestActorCriticPolicy(unittest.TestCase):
         self.assertEqual(policy.gamma, self.policy_params['gamma'])
         self.assertEqual(policy.layer_norm, self.policy_params['layer_norm'])
         self.assertEqual(policy.act_fun, self.policy_params['act_fun'])
-        self.assertEqual(policy.use_huber, self.policy_params['use_hube'])
+        self.assertEqual(policy.use_huber, self.policy_params['use_huber'])
 
         # Check that the abstract class has all the required methods.
         self.assertRaises(NotImplementedError, policy.initialize)
@@ -76,7 +76,26 @@ class TestActorCriticPolicy(unittest.TestCase):
         2. for 1-D observations and contexts
         3. for 2-D observations and contexts
         """
-        pass  # TODO
+        policy = ActorCriticPolicy(**self.policy_params)
+
+        # test case 1
+        obs = np.array([0, 1, 2])
+        context = None
+        expected = obs
+        np.testing.assert_almost_equal(policy._get_obs(obs, context), expected)
+
+        # test case 2
+        obs = np.array([0, 1, 2])
+        context = np.array([3, 4])
+        expected = np.array([0, 1, 2, 3, 4])
+        np.testing.assert_almost_equal(policy._get_obs(obs, context), expected)
+
+        # test case 3
+        obs = np.array([[0, 1, 2]])
+        context = np.array([[3, 4]])
+        expected = np.array([[0, 1, 2, 3, 4]])
+        np.testing.assert_almost_equal(policy._get_obs(obs, context, axis=1),
+                                       expected)
 
     def test_get_ob_dim(self):
         """Check the functionality of the _get_ob_dim() method.
@@ -86,7 +105,17 @@ class TestActorCriticPolicy(unittest.TestCase):
         1. when the context is None
         2. when the context is not None
         """
-        pass  # TODO
+        policy = ActorCriticPolicy(**self.policy_params)
+
+        # test case 1
+        ob_space = Box(0, 1, shape=(2,))
+        co_space = None
+        self.assertTupleEqual(policy._get_ob_dim(ob_space, co_space), (2,))
+
+        # test case 2
+        ob_space = Box(0, 1, shape=(2,))
+        co_space = Box(0, 1, shape=(3,))
+        self.assertTupleEqual(policy._get_ob_dim(ob_space, co_space), (5,))
 
     def test_layer(self):
         """Check the functionality of the _layer() method.
@@ -100,6 +129,21 @@ class TestActorCriticPolicy(unittest.TestCase):
            constant initializer)
         5. layer_norm is applied if requested
         """
+        # policy = ActorCriticPolicy(**self.policy_params)
+
+        # test case 1
+        pass  # TODO
+
+        # test case 2
+        pass  # TODO
+
+        # test case 3
+        pass  # TODO
+
+        # test case 4
+        pass  # TODO
+
+        # test case 5
         pass  # TODO
 
     def test_setup_target_updates(self):
@@ -108,7 +152,27 @@ class TestActorCriticPolicy(unittest.TestCase):
         This test validates both the init and soft update procedures generated
         by the tested method.
         """
-        pass  # TODO
+        policy = ActorCriticPolicy(**self.policy_params)
+
+        _ = tf.Variable(initial_value=[[1, 1, 1, 1]], dtype=tf.float32,
+                        name="0")
+        val1 = tf.Variable(initial_value=[[0, 0, 0, 0]], dtype=tf.float32,
+                           name="1")
+
+        # Initialize the variables of the policy.
+        policy.sess.run(tf.compat.v1.global_variables_initializer())
+
+        init, soft = policy._setup_target_updates("0", "1", None, 0.1, 0)
+
+        # test soft update
+        policy.sess.run(soft)
+        expected = np.array([[0.1, 0.1, 0.1, 0.1]])
+        np.testing.assert_almost_equal(policy.sess.run(val1), expected)
+
+        # test init update
+        policy.sess.run(init)
+        expected = np.array([[1, 1, 1, 1]])
+        np.testing.assert_almost_equal(policy.sess.run(val1), expected)
 
     def test_remove_fingerprint(self):
         """Check the functionality of the _remove_fingerprint() method.
@@ -118,7 +182,19 @@ class TestActorCriticPolicy(unittest.TestCase):
         1. for an additional_dim of zero
         2. for an additional_dim greater than zero
         """
-        pass  # TODO
+        policy = ActorCriticPolicy(**self.policy_params)
+
+        # test case 1
+        val = tf.constant(value=[[1, 2, 3, 4]], dtype=tf.float32)
+        new_val = policy._remove_fingerprint(val, 4, 2, 0)
+        expected = np.array([[1, 2, 0, 0]])
+        np.testing.assert_almost_equal(policy.sess.run(new_val), expected)
+
+        # test case 2
+        val = tf.constant(value=[[1, 2, 3, 4]], dtype=tf.float32)
+        new_val = policy._remove_fingerprint(val, 3, 2, 1)
+        expected = np.array([[1, 0, 0, 4]])
+        np.testing.assert_almost_equal(policy.sess.run(new_val), expected)
 
 
 class TestTD3FeedForwardPolicy(unittest.TestCase):
