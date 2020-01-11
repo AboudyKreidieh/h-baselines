392db84252630a47d7b56e99aa277b7fa6944a3a
AboudyKreidieh <akreidieh@gmail.com>
2020-01-11 23:38:28 +0200

    added several tests

--- tests/fast_tests/test_policy.py
+++ tests/fast_tests/test_policy.py
@@ -7,7 +7,7 @@ from gym.spaces import Box
 from hbaselines.utils.tf_util import get_trainable_vars
 from hbaselines.fcnet.base import ActorCriticPolicy
 from hbaselines.fcnet.td3 import FeedForwardPolicy as TD3FeedForwardPolicy
-# from hbaselines.fcnet.sac import FeedForwardPolicy as SACFeedForwardPolicy
+from hbaselines.fcnet.sac import FeedForwardPolicy as SACFeedForwardPolicy
 from hbaselines.goal_conditioned.td3 import GoalConditionedPolicy
 from hbaselines.algorithms.off_policy import SAC_PARAMS, TD3_PARAMS
 from hbaselines.algorithms.off_policy import FEEDFORWARD_PARAMS
@@ -18,8 +18,10 @@ class TestActorCriticPolicy(unittest.TestCase):
     """Test ActorCriticPolicy in hbaselines/fcnet/base.py."""
 
     def setUp(self):
+        sess = tf.compat.v1.Session()
+
         self.policy_params = {
-            'sess': tf.compat.v1.Session(),
+            'sess': sess,
             'ac_space': Box(low=-1, high=1, shape=(1,), dtype=np.float32),
             'ob_space': Box(low=-2, high=2, shape=(2,), dtype=np.float32),
             'co_space': Box(low=-3, high=3, shape=(3,), dtype=np.float32),
@@ -216,24 +218,20 @@ class TestTD3FeedForwardPolicy(unittest.TestCase):
         self.policy_params['sess'].close()
         del self.policy_params
 
+        # Clear the graph.
+        tf.compat.v1.reset_default_graph()
+
     def test_init(self):
-        """Validate that the graph and variables are initialized properly."""
+        """Check the functionality of the __init__() method.
+
+        This method is tested for the following features:
+
+        1. The proper structure graph was generated.
+        2. All input placeholders are correct.
+        """
         policy = TD3FeedForwardPolicy(**self.policy_params)
 
-        # Check that the abstract class has all the required attributes.
-        self.assertEqual(policy.buffer_size, self.policy_params['buffer_size'])
-        self.assertEqual(policy.batch_size, self.policy_params['batch_size'])
-        self.assertEqual(policy.actor_lr, self.policy_params['actor_lr'])
-        self.assertEqual(policy.critic_lr, self.policy_params['critic_lr'])
-        self.assertEqual(policy.verbose,  self.policy_params['verbose'])
-        self.assertEqual(policy.tau,  self.policy_params['tau'])
-        self.assertEqual(policy.gamma,  self.policy_params['gamma'])
-        self.assertEqual(policy.layer_norm, self.policy_params['layer_norm'])
-        self.assertListEqual(policy.layers, [256, 256])
-        self.assertEqual(policy.act_fun, self.policy_params['act_fun'])
-
-        # Check that all trainable variables have been created in the
-        # TensorFlow graph.
+        # test case 1
         self.assertListEqual(
             sorted([var.name for var in get_trainable_vars()]),
             ['model/pi/fc0/bias:0',
@@ -274,7 +272,7 @@ class TestTD3FeedForwardPolicy(unittest.TestCase):
              'target/qf_1/qf_output/kernel:0']
         )
 
-        # Check that all the input placeholders were properly created.
+        # test case 2
         self.assertEqual(
             tuple(v.__int__() for v in policy.terminals1.shape),
             (None, 1))
@@ -293,20 +291,67 @@ class TestTD3FeedForwardPolicy(unittest.TestCase):
             (None, self.policy_params['ob_space'].shape[0] +
              self.policy_params['co_space'].shape[0]))
 
-        # Clear the graph.
-        tf.compat.v1.reset_default_graph()
-
     def test_initialize(self):
-        """Test that the target variables are properly initialized."""
-        pass  # TODO
+        """Check the functionality of the initialize() method.
 
-    def test_optimization(self):
-        """Test the losses and gradient update steps."""
-        pass  # TODO
+        This test validates that the target variables are properly initialized
+        when initialize is called.
+        """
+        policy = TD3FeedForwardPolicy(**self.policy_params)
 
-    def test_update_target(self):
-        """Test the soft and init target updates."""
-        pass  # TODO
+        # Initialize the variables of the policy.
+        policy.sess.run(tf.compat.v1.global_variables_initializer())
+
+        # Run the initialize method.
+        policy.initialize()
+
+        model_var_list = [
+            'model/pi/fc0/bias:0',
+            'model/pi/fc0/kernel:0',
+            'model/pi/fc1/bias:0',
+            'model/pi/fc1/kernel:0',
+            'model/pi/output/bias:0',
+            'model/pi/output/kernel:0',
+            'model/qf_0/fc0/bias:0',
+            'model/qf_0/fc0/kernel:0',
+            'model/qf_0/fc1/bias:0',
+            'model/qf_0/fc1/kernel:0',
+            'model/qf_0/qf_output/bias:0',
+            'model/qf_0/qf_output/kernel:0',
+            'model/qf_1/fc0/bias:0',
+            'model/qf_1/fc0/kernel:0',
+            'model/qf_1/fc1/bias:0',
+            'model/qf_1/fc1/kernel:0',
+            'model/qf_1/qf_output/bias:0',
+            'model/qf_1/qf_output/kernel:0',
+        ]
+
+        target_var_list = [
+            'target/pi/fc0/bias:0',
+            'target/pi/fc0/kernel:0',
+            'target/pi/fc1/bias:0',
+            'target/pi/fc1/kernel:0',
+            'target/pi/output/bias:0',
+            'target/pi/output/kernel:0',
+            'target/qf_0/fc0/bias:0',
+            'target/qf_0/fc0/kernel:0',
+            'target/qf_0/fc1/bias:0',
+            'target/qf_0/fc1/kernel:0',
+            'target/qf_0/qf_output/bias:0',
+            'target/qf_0/qf_output/kernel:0',
+            'target/qf_1/fc0/bias:0',
+            'target/qf_1/fc0/kernel:0',
+            'target/qf_1/fc1/bias:0',
+            'target/qf_1/fc1/kernel:0',
+            'target/qf_1/qf_output/bias:0',
+            'target/qf_1/qf_output/kernel:0'
+        ]
+
+        for model, target in zip(model_var_list, target_var_list):
+            with tf.variable_scope(tf.get_variable_scope(), reuse=True):
+                model_val = policy.sess.run(model)
+                target_val = policy.sess.run(target)
+            np.testing.assert_almost_equal(model_val, target_val)
 
     def test_store_transition(self):
         """Test the `store_transition` method."""
@@ -332,27 +377,159 @@ class TestSACFeedForwardPolicy(unittest.TestCase):
         self.policy_params['sess'].close()
         del self.policy_params
 
-    def test_init(self):
-        """Validate that the graph and variables are initialized properly."""
-        # policy = SACFeedForwardPolicy(**self.policy_params)
+        # Clear the graph.
+        tf.compat.v1.reset_default_graph()
 
-        pass  # TODO
+    def test_init(self):
+        """Check the functionality of the __init__() method.
+
+        This method is tested for the following features:
+
+        1. The proper structure graph was generated.
+        2. All input placeholders are correct.
+        3. self.log_alpha is initialized to zero
+        4. self.target_entropy is initialized as specified, with the special
+           (None) case as well
+        """
+        policy = SACFeedForwardPolicy(**self.policy_params)
+
+        # test case 1
+        self.assertListEqual(
+            sorted([var.name for var in get_trainable_vars()]),
+            ['model/log_alpha:0',
+             'model/pi/fc0/bias:0',
+             'model/pi/fc0/kernel:0',
+             'model/pi/fc1/bias:0',
+             'model/pi/fc1/kernel:0',
+             'model/pi/log_std/bias:0',
+             'model/pi/log_std/kernel:0',
+             'model/pi/mean/bias:0',
+             'model/pi/mean/kernel:0',
+             'model/value_fns/qf1/fc0/bias:0',
+             'model/value_fns/qf1/fc0/kernel:0',
+             'model/value_fns/qf1/fc1/bias:0',
+             'model/value_fns/qf1/fc1/kernel:0',
+             'model/value_fns/qf1/qf_output/bias:0',
+             'model/value_fns/qf1/qf_output/kernel:0',
+             'model/value_fns/qf2/fc0/bias:0',
+             'model/value_fns/qf2/fc0/kernel:0',
+             'model/value_fns/qf2/fc1/bias:0',
+             'model/value_fns/qf2/fc1/kernel:0',
+             'model/value_fns/qf2/qf_output/bias:0',
+             'model/value_fns/qf2/qf_output/kernel:0',
+             'model/value_fns/vf/fc0/bias:0',
+             'model/value_fns/vf/fc0/kernel:0',
+             'model/value_fns/vf/fc1/bias:0',
+             'model/value_fns/vf/fc1/kernel:0',
+             'model/value_fns/vf/vf_output/bias:0',
+             'model/value_fns/vf/vf_output/kernel:0',
+             'target/value_fns/vf/fc0/bias:0',
+             'target/value_fns/vf/fc0/kernel:0',
+             'target/value_fns/vf/fc1/bias:0',
+             'target/value_fns/vf/fc1/kernel:0',
+             'target/value_fns/vf/vf_output/bias:0',
+             'target/value_fns/vf/vf_output/kernel:0']
+        )
+
+        # test case 2
+        self.assertEqual(
+            tuple(v.__int__() for v in policy.terminals1.shape),
+            (None, 1))
+        self.assertEqual(
+            tuple(v.__int__() for v in policy.rew_ph.shape),
+            (None, 1))
+        self.assertEqual(
+            tuple(v.__int__() for v in policy.action_ph.shape),
+            (None, self.policy_params['ac_space'].shape[0]))
+        self.assertEqual(
+            tuple(v.__int__() for v in policy.obs_ph.shape),
+            (None, self.policy_params['ob_space'].shape[0] +
+             self.policy_params['co_space'].shape[0]))
+        self.assertEqual(
+            tuple(v.__int__() for v in policy.obs1_ph.shape),
+            (None, self.policy_params['ob_space'].shape[0] +
+             self.policy_params['co_space'].shape[0]))
+
+        # Initialize the variables of the policy.
+        policy.sess.run(tf.compat.v1.global_variables_initializer())
+
+        # test case 3
+        self.assertEqual(policy.sess.run(policy.log_alpha), 0.0)
+
+        # test case 4a
+        self.assertEqual(policy.target_entropy,
+                         -self.policy_params['ac_space'].shape[0])
+
+        # Clear the graph.
+        tf.compat.v1.reset_default_graph()
+
+        # test case 4b
+        self.policy_params['target_entropy'] = 5
+        policy = SACFeedForwardPolicy(**self.policy_params)
+        self.assertEqual(policy.target_entropy,
+                         self.policy_params['target_entropy'])
 
     def test_gaussian_likelihood(self):
-        """TODO."""
-        pass  # TODO
+        """Check the functionality of the _gaussian_likelihood() method."""
+        policy = SACFeedForwardPolicy(**self.policy_params)
+
+        input_ = tf.constant([[0, 1, 2]], dtype=tf.float32)
+        mu_ = tf.constant([[0, 0, 0]], dtype=tf.float32)
+        log_std = tf.constant([[-4, -3, -2]], dtype=tf.float32)
+        val = policy._gaussian_likelihood(input_, mu_, log_std)
+        expected = -304.65784
+
+        self.assertAlmostEqual(policy.sess.run(val)[0], expected, places=4)
 
     def test_apply_squashing(self):
-        """TODO."""
+        """Check the functionality of the _apply_squashing() method."""
         pass  # TODO
 
     def test_initialize(self):
-        """Test that the target variables are properly initialized."""
+        """Check the functionality of the initialize() method.
+
+        This test validates that the target variables are properly initialized
+        when initialize is called.
+        """
+        policy = SACFeedForwardPolicy(**self.policy_params)
+
+        # Initialize the variables of the policy.
+        policy.sess.run(tf.compat.v1.global_variables_initializer())
+
+        # Run the initialize method.
+        policy.initialize()
+
+        model_var_list = [
+            'model/value_fns/vf/fc0/kernel:0',
+            'model/value_fns/vf/fc0/bias:0',
+            'model/value_fns/vf/fc1/kernel:0',
+            'model/value_fns/vf/fc1/bias:0',
+            'model/value_fns/vf/vf_output/kernel:0',
+            'model/value_fns/vf/vf_output/bias:0',
+        ]
+
+        target_var_list = [
+            'target/value_fns/vf/fc0/kernel:0',
+            'target/value_fns/vf/fc0/bias:0',
+            'target/value_fns/vf/fc1/kernel:0',
+            'target/value_fns/vf/fc1/bias:0',
+            'target/value_fns/vf/vf_output/kernel:0',
+            'target/value_fns/vf/vf_output/bias:0',
+        ]
+
+        for model, target in zip(model_var_list, target_var_list):
+            with tf.variable_scope(tf.get_variable_scope(), reuse=True):
+                model_val = policy.sess.run(model)
+                target_val = policy.sess.run(target)
+            np.testing.assert_almost_equal(model_val, target_val)
+
+    def test_store_transition(self):
+        """Check the functionality of the store_transition() method."""
         pass  # TODO
 
 
-class TestGoalConditionedPolicy(unittest.TestCase):
-    """Test GoalConditionedPolicy in hbaselines/goal_conditioned/td3.py."""
+class TestBaseGoalConditionedPolicy(unittest.TestCase):
+    """Test GoalConditionedPolicy in hbaselines/goal_conditioned/base.py."""
 
     def setUp(self):
         self.policy_params = {
