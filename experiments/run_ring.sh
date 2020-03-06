4bb4ee4c80481ce850c6a6a34d21a747d5fa2230
AboudyKreidieh <akreidieh@gmail.com>
2020-03-06 14:05:53 -0800

    some additions

---
+++ experiments/run_ring.sh
@@ -0,0 +1,65 @@
+#!/bin/bash
+
+# SAC
+
+python run_fcnet.py "ring0" --gamma 0.999 --alg "SAC" --nb_rollout_steps 50 --seed 1
+python run_fcnet.py "ring0" --gamma 0.999 --alg "SAC" --nb_rollout_steps 50 --seed 2
+python run_fcnet.py "ring0" --gamma 0.999 --alg "SAC" --nb_rollout_steps 50 --seed 3
+python run_fcnet.py "ring0" --gamma 0.999 --alg "SAC" --nb_rollout_steps 50 --seed 4
+python run_fcnet.py "ring0" --gamma 0.999 --alg "SAC" --nb_rollout_steps 50 --seed 5
+
+python run_fcnet.py "ring0" --gamma 0.999 --alg "SAC" --nb_rollout_steps 25 --seed 1
+python run_fcnet.py "ring0" --gamma 0.999 --alg "SAC" --nb_rollout_steps 25 --seed 2
+python run_fcnet.py "ring0" --gamma 0.999 --alg "SAC" --nb_rollout_steps 25 --seed 3
+python run_fcnet.py "ring0" --gamma 0.999 --alg "SAC" --nb_rollout_steps 25 --seed 4
+python run_fcnet.py "ring0" --gamma 0.999 --alg "SAC" --nb_rollout_steps 25 --seed 5
+
+python run_fcnet.py "ring0" --gamma 0.999 --alg "SAC" --nb_rollout_steps 10 --seed 1
+python run_fcnet.py "ring0" --gamma 0.999 --alg "SAC" --nb_rollout_steps 10 --seed 2
+python run_fcnet.py "ring0" --gamma 0.999 --alg "SAC" --nb_rollout_steps 10 --seed 3
+python run_fcnet.py "ring0" --gamma 0.999 --alg "SAC" --nb_rollout_steps 10 --seed 4
+python run_fcnet.py "ring0" --gamma 0.999 --alg "SAC" --nb_rollout_steps 10 --seed 5
+
+python run_fcnet.py "ring0" --gamma 0.999 --alg "SAC" --nb_rollout_steps 5 --seed 1
+python run_fcnet.py "ring0" --gamma 0.999 --alg "SAC" --nb_rollout_steps 5 --seed 2
+python run_fcnet.py "ring0" --gamma 0.999 --alg "SAC" --nb_rollout_steps 5 --seed 3
+python run_fcnet.py "ring0" --gamma 0.999 --alg "SAC" --nb_rollout_steps 5 --seed 4
+python run_fcnet.py "ring0" --gamma 0.999 --alg "SAC" --nb_rollout_steps 5 --seed 5
+
+python run_fcnet.py "ring0" --gamma 0.999 --alg "SAC" --seed 1
+python run_fcnet.py "ring0" --gamma 0.999 --alg "SAC" --seed 2
+python run_fcnet.py "ring0" --gamma 0.999 --alg "SAC" --seed 3
+python run_fcnet.py "ring0" --gamma 0.999 --alg "SAC" --seed 4
+python run_fcnet.py "ring0" --gamma 0.999 --alg "SAC" --seed 5
+
+# TD3
+
+python run_fcnet.py "ring0" --gamma 0.999 --alg "TD3" --nb_rollout_steps 50 --seed 1
+python run_fcnet.py "ring0" --gamma 0.999 --alg "TD3" --nb_rollout_steps 50 --seed 2
+python run_fcnet.py "ring0" --gamma 0.999 --alg "TD3" --nb_rollout_steps 50 --seed 3
+python run_fcnet.py "ring0" --gamma 0.999 --alg "TD3" --nb_rollout_steps 50 --seed 4
+python run_fcnet.py "ring0" --gamma 0.999 --alg "TD3" --nb_rollout_steps 50 --seed 5
+
+python run_fcnet.py "ring0" --gamma 0.999 --alg "TD3" --nb_rollout_steps 25 --seed 1
+python run_fcnet.py "ring0" --gamma 0.999 --alg "TD3" --nb_rollout_steps 25 --seed 2
+python run_fcnet.py "ring0" --gamma 0.999 --alg "TD3" --nb_rollout_steps 25 --seed 3
+python run_fcnet.py "ring0" --gamma 0.999 --alg "TD3" --nb_rollout_steps 25 --seed 4
+python run_fcnet.py "ring0" --gamma 0.999 --alg "TD3" --nb_rollout_steps 25 --seed 5
+
+python run_fcnet.py "ring0" --gamma 0.999 --alg "TD3" --nb_rollout_steps 10 --seed 1
+python run_fcnet.py "ring0" --gamma 0.999 --alg "TD3" --nb_rollout_steps 10 --seed 2
+python run_fcnet.py "ring0" --gamma 0.999 --alg "TD3" --nb_rollout_steps 10 --seed 3
+python run_fcnet.py "ring0" --gamma 0.999 --alg "TD3" --nb_rollout_steps 10 --seed 4
+python run_fcnet.py "ring0" --gamma 0.999 --alg "TD3" --nb_rollout_steps 10 --seed 5
+
+python run_fcnet.py "ring0" --gamma 0.999 --alg "TD3" --nb_rollout_steps 5 --seed 1
+python run_fcnet.py "ring0" --gamma 0.999 --alg "TD3" --nb_rollout_steps 5 --seed 2
+python run_fcnet.py "ring0" --gamma 0.999 --alg "TD3" --nb_rollout_steps 5 --seed 3
+python run_fcnet.py "ring0" --gamma 0.999 --alg "TD3" --nb_rollout_steps 5 --seed 4
+python run_fcnet.py "ring0" --gamma 0.999 --alg "TD3" --nb_rollout_steps 5 --seed 5
+
+python run_fcnet.py "ring0" --gamma 0.999 --alg "TD3" --seed 1
+python run_fcnet.py "ring0" --gamma 0.999 --alg "TD3" --seed 2
+python run_fcnet.py "ring0" --gamma 0.999 --alg "TD3" --seed 3
+python run_fcnet.py "ring0" --gamma 0.999 --alg "TD3" --seed 4
+python run_fcnet.py "ring0" --gamma 0.999 --alg "TD3" --seed 5
