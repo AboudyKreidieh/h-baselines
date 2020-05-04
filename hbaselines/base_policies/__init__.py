6e073b7c17fdbab4d00202379ba465f82aeec2bb
Aboudy Kreidieh <akreidieh@gmail.com>
2020-05-03 23:59:08 -0700

    added base imitation learning policy (#136)
    * added base imitation learning policy
    
    * Apply suggestions from code review
    
    * Apply suggestions from code review

--- hbaselines/base_policies/__init__.py
+++ hbaselines/base_policies/__init__.py
@@ -1,6 +1,8 @@
 """Init file for the base policies submodule."""
 from hbaselines.base_policies.actor_critic import ActorCriticPolicy
+from hbaselines.base_policies.imitation import ImitationLearningPolicy
 
 __all__ = [
-    "ActorCriticPolicy"
+    "ActorCriticPolicy",
+    "ImitationLearningPolicy"
 ]
