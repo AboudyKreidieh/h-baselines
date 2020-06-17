2209f9ff2ce7b54884e4c1eda435158e088ad9a3
AboudyKreidieh <akreidieh@gmail.com>
2020-06-17 13:30:06 -0700

    updated setup process

--- setup.py
+++ setup.py
@@ -1,8 +1,13 @@
 #!/usr/bin/env python3
 # flake8: noqa
 """Setup script for the h-baselines repository."""
-from os.path import dirname, realpath
-from setuptools import find_packages, setup
+from os.path import dirname
+from os.path import realpath
+from setuptools import find_packages
+from setuptools import setup
+from setuptools import Distribution
+import setuptools.command.build_ext as _build_ext
+import subprocess
 from hbaselines.version import __version__
 
 
@@ -12,9 +17,28 @@ def _read_requirements_file():
         return [line.strip() for line in f]
 
 
+class BuildExt(_build_ext.build_ext):
+    """External buid commands."""
+
+    def run(self):
+        """Install traci wheels."""
+        subprocess.check_call(['pip', 'install', 'ray[tune]'])
+
+
+class BinaryDistribution(Distribution):
+    """See parent class."""
+
+    @staticmethod
+    def has_ext_modules():
+        """Return True for external modules."""
+        return True
+
+
 setup(
     name='h-baselines',
     version=__version__,
+    distclass=BinaryDistribution,
+    cmdclass={"build_ext": BuildExt},
     packages=find_packages(),
     install_requires=_read_requirements_file(),
     description='h-baselines: a repository of high-performing and benchmarked '
