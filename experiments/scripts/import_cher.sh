3fcbc9429c51c5ba7ef789ed72eaa34aa37f4289
AboudyKreidieh <akreidieh@gmail.com>
2020-06-29 09:58:06 -0700

    uncommented all import operations

--- experiments/scripts/import_cher.sh
+++ experiments/scripts/import_cher.sh
@@ -9,34 +9,34 @@ mkdir "$DIR/../pretrained" && cd "$DIR/../pretrained"
 # TD3
 mkdir TD3 && pushd TD3
 wget https://cher-results.s3.us-east-2.amazonaws.com/TD3/AntGather.zip && unzip AntGather.zip && rm AntGather.zip
-#wget https://cher-results.s3.us-east-2.amazonaws.com/TD3/AntMaze.zip && unzip AntMaze.zip && rm AntMaze.zip
-#wget https://cher-results.s3.us-east-2.amazonaws.com/TD3/highway-v1.zip && unzip i210-v1.zip && rm highway-v1.zip
+wget https://cher-results.s3.us-east-2.amazonaws.com/TD3/AntMaze.zip && unzip AntMaze.zip && rm AntMaze.zip
+wget https://cher-results.s3.us-east-2.amazonaws.com/TD3/highway-v1.zip && unzip i210-v1.zip && rm highway-v1.zip
 popd
 
 # HRL
 mkdir HRL && pushd HRL
 wget https://cher-results.s3.us-east-2.amazonaws.com/HRL/AntGather.zip && unzip AntGather.zip && rm AntGather.zip
-#wget https://cher-results.s3.us-east-2.amazonaws.com/HRL/AntMaze.zip && unzip AntMaze.zip && rm AntMaze.zip
-#wget https://cher-results.s3.us-east-2.amazonaws.com/HRL/highway-v1.zip && unzip highway-v1.zip && rm highway-v1.zip
+wget https://cher-results.s3.us-east-2.amazonaws.com/HRL/AntMaze.zip && unzip AntMaze.zip && rm AntMaze.zip
+wget https://cher-results.s3.us-east-2.amazonaws.com/HRL/highway-v1.zip && unzip highway-v1.zip && rm highway-v1.zip
 popd
 
 # HIRO
 mkdir HIRO && pushd HIRO
 wget https://cher-results.s3.us-east-2.amazonaws.com/HIRO/AntGather.zip && unzip AntGather.zip && rm AntGather.zip
-#wget https://cher-results.s3.us-east-2.amazonaws.com/HIRO/AntMaze.zip && unzip AntMaze.zip && rm AntMaze.zip
-#wget https://cher-results.s3.us-east-2.amazonaws.com/HIRO/highway-v1.zip && unzip highway-v1.zip && rm highway-v1.zip
+wget https://cher-results.s3.us-east-2.amazonaws.com/HIRO/AntMaze.zip && unzip AntMaze.zip && rm AntMaze.zip
+wget https://cher-results.s3.us-east-2.amazonaws.com/HIRO/highway-v1.zip && unzip highway-v1.zip && rm highway-v1.zip
 popd
 
 # HAC
 mkdir HAC && pushd HAC
 wget https://cher-results.s3.us-east-2.amazonaws.com/HAC/AntGather.zip && unzip AntGather.zip && rm AntGather.zip
-#wget https://cher-results.s3.us-east-2.amazonaws.com/HAC/AntMaze.zip && unzip AntMaze.zip && rm AntMaze.zip
-#wget https://cher-results.s3.us-east-2.amazonaws.com/HAC/highway-v1.zip && unzip highway-v1.zip && rm highway-v1.zip
+wget https://cher-results.s3.us-east-2.amazonaws.com/HAC/AntMaze.zip && unzip AntMaze.zip && rm AntMaze.zip
+wget https://cher-results.s3.us-east-2.amazonaws.com/HAC/highway-v1.zip && unzip highway-v1.zip && rm highway-v1.zip
 popd
 
 # CHER
 mkdir CHER && pushd CHER
 wget https://cher-results.s3.us-east-2.amazonaws.com/CHER/AntGather.zip && unzip AntGather.zip && rm AntGather.zip
-#wget https://cher-results.s3.us-east-2.amazonaws.com/CHER/AntMaze.zip && unzip AntMaze.zip && rm AntMaze.zip
-#wget https://cher-results.s3.us-east-2.amazonaws.com/CHER/highway-v1.zip && unzip highway-v1.zip && rm highway-v1.zip
+wget https://cher-results.s3.us-east-2.amazonaws.com/CHER/AntMaze.zip && unzip AntMaze.zip && rm AntMaze.zip
+wget https://cher-results.s3.us-east-2.amazonaws.com/CHER/highway-v1.zip && unzip highway-v1.zip && rm highway-v1.zip
 popd
