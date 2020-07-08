21975cc7495d4a78360b7cbde0174f2012a82512
AboudyKreidieh <akreidieh@gmail.com>
2020-07-08 14:58:52 -0700

    minor changes

--- experiments/plot.py
+++ experiments/plot.py
@@ -6,22 +6,43 @@ import matplotlib.pyplot as plt
 import argparse
 import os
 import seaborn as sns
+import sys
 
 sns.set()
 
 
-if __name__ == "__main__":
-
+def parse_options(args):
+    """Parse plotting options user can specify in command line."""
     parser = argparse.ArgumentParser()
-    parser.add_argument("--files", type=str, nargs="+")
-    parser.add_argument("--names", type=str, nargs="+")
-    parser.add_argument("--out", type=str, default="out.png")
-    parser.add_argument("--y", type=str, default="success_rate")
-    parser.add_argument("--x", type=str, default="total_step")
-    args = parser.parse_args()
+
+    parser.add_argument(
+        "--files", type=str, nargs="+",
+        help="TODO")
+    parser.add_argument(
+        "--names", type=str, nargs="+",
+        help="TODO")
+    parser.add_argument(
+        "--out", type=str, default="out.png",
+        help="TODO")
+    parser.add_argument(
+        "--y", type=str, default="success_rate",
+        help="TODO")
+    parser.add_argument(
+        "--x", type=str, default="total_step",
+        help="TODO")
+    parser.add_argument(
+        "--save_format", type=str, default="png",
+        help="The format the image is to be saved in. For example: 'png', "
+             "'svg', etc. Defaults to png.")
+
+    return parser.parse_args(args)
+
+
+if __name__ == "__main__":
+    flags = parse_options(sys.argv[1:])
 
     all_results = defaultdict(list)
-    for path, name in zip(args.files, args.names):
+    for path, name in zip(flags.files, flags.names):
         results = defaultdict(list)
 
         for filepath in tf.io.gfile.glob(os.path.join(path, "*/eval*.csv")):
@@ -34,12 +55,10 @@ if __name__ == "__main__":
             all_results[key].append(data)
 
     for key in all_results.keys():
-
         df = pd.concat(all_results[key])
         stop = df.groupby("name")["total_step"].max().min()
         df = df[df['total_step'] < stop]
         # df[args.x] = (df[args.x] / 10000).astype(int) * 10000
         plt.clf()
-        ax = sns.lineplot(x=args.x, y=args.y, hue="name",
-                          data=df)
-        plt.savefig(args.out.replace(".", "_{}.".format(key)))
+        ax = sns.lineplot(x=flags.x, y=flags.y, hue="name", data=df)
+        plt.savefig(flags.out.replace(".", "_{}.".format(key)))
