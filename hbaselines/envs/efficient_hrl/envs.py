6187e9b4e18ceb1cd6944dcc25c2ab3bbcf2489a
Aboudy Kreidieh <akreidieh@gmail.com>
2020-07-11 14:46:59 -0700

    Humanoid (#174)
    * merged humanoid code from brandon's branch
    
    * some missing code
    
    * expanded test coverage
    
    * test for ray_segment_intersect
    
    * modified assertion
    
    * another assertion

--- hbaselines/envs/efficient_hrl/envs.py
+++ hbaselines/envs/efficient_hrl/envs.py
@@ -5,6 +5,7 @@ from gym.spaces import Box
 
 from hbaselines.utils.reward_fns import negative_distance
 from hbaselines.envs.efficient_hrl.ant_maze_env import AntMazeEnv
+from hbaselines.envs.efficient_hrl.humanoid_maze_env import HumanoidMazeEnv
 
 # scale to the contextual reward. Does not affect the environmental reward.
 REWARD_SCALE = 0.1
@@ -148,7 +149,8 @@ class UniversalAntMazeEnv(AntMazeEnv):
                     # Use the original context as the context space. It is a
                     # fixed value in this case.
                     return Box(low=np.asarray(self.context_range),
-                               high=np.asarray(self.context_range))
+                               high=np.asarray(self.context_range),
+                               dtype=np.float32)
         else:
             return None
 
@@ -239,6 +241,230 @@ class UniversalAntMazeEnv(AntMazeEnv):
         return self.prev_obs
 
 
+class UniversalHumanoidMazeEnv(HumanoidMazeEnv):
+    """Universal environment variant of HumanoidMazeEnv.
+
+    This environment extends the generic gym environment by including contexts,
+    or goals. The goals are added to the observation, and an additional
+    contextual reward is included to the generic rewards.
+    """
+
+    def __init__(self,
+                 maze_id,
+                 contextual_reward,
+                 use_contexts=False,
+                 random_contexts=False,
+                 context_range=None,
+                 maze_size_scaling=4,
+                 top_down_view=False,
+                 image_size=32,
+                 horizon=1000):
+        """Initialize the Universal environment.
+
+        Parameters
+        ----------
+        maze_id : str
+            the type of maze environment. One of "Maze", "Push", or "Fall"
+        contextual_reward : function
+            a reward function that takes as input (states, goals, next_states)
+            and returns a float reward and whether the goal has been achieved
+        use_contexts : bool, optional
+            specifies whether to add contexts to the observations and add the
+            contextual rewards
+        random_contexts : bool
+            specifies whether the context is a single value, or a random set of
+            values between some range
+        context_range : [float] or [(float, float)] or [[float]]
+            one of the following three:
+            1. the desired context / goal
+            2. the (lower, upper) bound tuple for each dimension of the goal
+            3. a list of desired contexts / goals. Goals are sampled from these
+               list of possible goals
+        top_down_view : bool
+            specifies whether the observation should have an image prepended
+            useful for training convolutional policies
+        image_size: int
+            determines the width and height of the rendered image
+        horizon : float, optional
+            time horizon
+
+        Raises
+        ------
+        AssertionError
+            If the context_range is not the right form based on whether
+            contexts are a single value or random across a range.
+        """
+        # Initialize the maze variant of the environment.
+        super(UniversalHumanoidMazeEnv, self).__init__(
+            maze_id=maze_id,
+            maze_height=0.5,
+            maze_size_scaling=maze_size_scaling,
+            n_bins=0,
+            sensor_range=3.,
+            sensor_span=2 * np.pi,
+            observe_blocks=False,
+            put_spin_near_agent=False,
+            top_down_view=top_down_view,
+            image_size=image_size,
+            manual_collision=False
+        )
+
+        self.horizon = horizon
+        self.step_number = 0
+
+        # contextual variables
+        self.use_contexts = use_contexts
+        self.random_contexts = random_contexts
+        self.context_range = context_range
+        self.contextual_reward = contextual_reward
+        self.current_context = None
+
+        # a hack to deal with previous observations in the reward
+        self.prev_obs = None
+
+        # Check that context_range is the right form based on whether contexts
+        # are a single value or random across a range.
+        if self.use_contexts:
+            if self.random_contexts:
+                assert all(isinstance(i, tuple) for i in self.context_range), \
+                    "When using random contexts, every element in " \
+                    "context_range, must be a tuple of (min,max) values."
+            else:
+                assert all(not isinstance(i, tuple) for i in
+                           self.context_range), \
+                    "When not using random contexts, every element in " \
+                    "context_range, must be a single value or a list of " \
+                    "values."
+
+    @property
+    def context_space(self):
+        """Return the shape and bounds of the contextual term."""
+        # Check if the environment is using contexts, and if not, return a None
+        # value as the context space.
+        if self.use_contexts:
+            # If the context space is random, use the min and max values of
+            # each context to specify the space range. Otherwise, the min and
+            # max values are both the deterministic context value.
+            if self.random_contexts:
+                context_low = []
+                context_high = []
+                for context_i in self.context_range:
+                    low, high = context_i
+                    context_low.append(low)
+                    context_high.append(high)
+                return Box(low=np.asarray(context_low),
+                           high=np.asarray(context_high),
+                           dtype=np.float32)
+            else:
+                # If there are a list of possible goals, use the min and max
+                # values of each index for the context space.
+                if isinstance(self.context_range[0], list):
+                    min_val = []
+                    max_val = []
+                    for i in range(len(self.context_range[0])):
+                        min_val.append(min(v[i] for v in self.context_range))
+                        max_val.append(max(v[i] for v in self.context_range))
+
+                    return Box(low=np.array(min_val),
+                               high=np.array(max_val),
+                               dtype=np.float32)
+                else:
+                    # Use the original context as the context space. It is a
+                    # fixed value in this case.
+                    return Box(low=np.asarray(self.context_range),
+                               high=np.asarray(self.context_range),
+                               dtype=np.float32)
+        else:
+            return None
+
+    def step(self, action):
+        """Advance the environment by one simulation step.
+
+        If the environment is using the contextual setting, an "is_success"
+        term is added to the info_dict to specify whether the objective has
+        been met.
+
+        Parameters
+        ----------
+        action : array_like
+            actions to be performed by the agent
+
+        Returns
+        -------
+        array_like
+            next observation
+        float
+            environmental reward
+        bool
+            done mask
+        dict
+            extra information dictionary
+        """
+        # Run environment update.
+        obs, rew, done, info = super(UniversalHumanoidMazeEnv, self).step(
+            action)
+
+        if self.use_contexts:
+            # Add success to the info dict
+            dist = self.contextual_reward(
+                states=self.prev_obs,
+                next_states=obs,
+                goals=self.current_context,
+            )
+            info["is_success"] = abs(dist) < DISTANCE_THRESHOLD * REWARD_SCALE
+
+            # Replace the reward with the contextual reward.
+            rew = dist
+
+        # Check if the time horizon has been met.
+        self.step_number += 1
+        done = done or self.step_number == self.horizon
+
+        return obs, rew, done, info
+
+    def reset(self):
+        """Reset the environment.
+
+        If the environment is using the contextual setting, a new context is
+        issued.
+
+        Returns
+        -------
+        array_like
+            initial observation
+        """
+        try:
+            self.prev_obs = super(UniversalHumanoidMazeEnv, self).reset()
+        except (NotImplementedError, AttributeError):
+            # for testing purposes
+            self.prev_obs = np.empty(1)
+
+        # Reset the step counter.
+        self.step_number = 0
+
+        if self.use_contexts:
+            if not self.random_contexts:
+                if isinstance(self.context_range[0], list):
+                    # In this case, sample on of the contexts as the next
+                    # environmental context.
+                    self.current_context = random.sample(self.context_range, 1)
+                    self.current_context = self.current_context[0]
+                else:
+                    # In this case, the context range is just the context.
+                    self.current_context = self.context_range
+            else:
+                # In this case, choose random values between the context range.
+                self.current_context = []
+                for range_i in self.context_range:
+                    minval, maxval = range_i
+                    self.current_context.append(random.uniform(minval, maxval))
+
+            # Convert to numpy array.
+            self.current_context = np.asarray(self.current_context)
+
+        return self.prev_obs
+
+
 class AntMaze(UniversalAntMazeEnv):
     """Ant Maze Environment.
 
@@ -295,6 +521,63 @@ class AntMaze(UniversalAntMazeEnv):
             context_range=context_range,
             top_down_view=False,
             maze_size_scaling=8)
+
+
+class HumanoidMaze(UniversalHumanoidMazeEnv):
+    """Humanoid Maze Environment.
+
+    In this task, immovable blocks are placed to confine the agent to a
+    U-shaped corridor. That is, blocks are placed everywhere except at (0,0),
+    (4,0), (8,0), (8,4), (8,8), (4,8), and (0,8). The agent is
+    initialized at position (0,0) and tasked at reaching a specific target
+    position. "Success" in this environment is defined as being within an L2
+    distance of 5 from the target.
+    """
+
+    def __init__(self,
+                 use_contexts=False,
+                 random_contexts=False,
+                 context_range=None):
+        """Initialize the Humanoid Maze environment.
+
+        Parameters
+        ----------
+        use_contexts : bool, optional
+            specifies whether to add contexts to the observations and add the
+            contextual rewards
+        random_contexts : bool
+            specifies whether the context is a single value, or a random set of
+            values between some range
+        context_range : [float] or [(float, float)] or [[float]]
+            the desired context / goal, or the (lower, upper) bound tuple for
+            each dimension of the goal
+
+        Raises
+        ------
+        AssertionError
+            If the context_range is not the right form based on whether
+            contexts are a single value or random across a range.
+        """
+        maze_id = "Maze"
+
+        def contextual_reward(states, goals, next_states):
+            return negative_distance(
+                states=states,
+                goals=goals,
+                next_states=next_states,
+                state_indices=[0, 1],
+                relative_context=False,
+                offset=0.0,
+                reward_scales=(2.0 / 11.313708499),
+                output_activation=np.exp)
+
+        super(HumanoidMaze, self).__init__(
+            maze_id=maze_id,
+            contextual_reward=contextual_reward,
+            use_contexts=use_contexts,
+            random_contexts=random_contexts,
+            context_range=context_range,
+            maze_size_scaling=4)
 
 
 class ImageAntMaze(UniversalAntMazeEnv):
---
+++ hbaselines/envs/efficient_hrl/humanoid.py
@@ -0,0 +1,132 @@
+"""Script containing the Humanoid environment."""
+import math
+import numpy as np
+import gym
+from gym import utils
+try:
+    from gym.envs.mujoco import mujoco_env
+except (ModuleNotFoundError, gym.error.DependencyNotInstalled):
+
+    def mujoco_env():
+        """Create a dummy environment for testing purposes."""
+        return None
+    setattr(mujoco_env, "MujocoEnv", gym.Env)
+
+
+def mass_center(model, sim):
+    """Compute the position of the agent's center of mass."""
+    mass = np.expand_dims(model.body_mass, 1)
+    xpos = sim.data.xipos
+    return (np.sum(mass * xpos, 0) / np.sum(mass))[0]
+
+
+def q_inv(a):
+    """Return the inverse of a quaternion."""
+    return [a[0], -a[1], -a[2], -a[3]]
+
+
+def q_mult(a, b):
+    """Multiply two quaternion."""
+    w = a[0] * b[0] - a[1] * b[1] - a[2] * b[2] - a[3] * b[3]
+    i = a[0] * b[1] + a[1] * b[0] + a[2] * b[3] - a[3] * b[2]
+    j = a[0] * b[2] - a[1] * b[3] + a[2] * b[0] + a[3] * b[1]
+    k = a[0] * b[3] + a[1] * b[2] - a[2] * b[1] + a[3] * b[0]
+    return [w, i, j, k]
+
+
+class HumanoidEnv(mujoco_env.MujocoEnv, utils.EzPickle):
+    """Humanoid mujoco environment."""
+
+    FILE = 'double_humanoid.xml'
+
+    def __init__(
+            self,
+            file_path):
+        """Create a humanoid agent."""
+        self._goal = None
+        mujoco_env.MujocoEnv.__init__(self, file_path, 5)
+        utils.EzPickle.__init__(self)
+
+    def _get_obs(self):
+        """Get the observation of the humanoid."""
+        data = self.sim.data
+        return np.concatenate([data.qpos[:24].flat,
+                               data.qvel[:23].flat,
+                               data.cinert[:14].flat,
+                               data.cvel[:14].flat,
+                               data.qfrc_actuator[:23].flat,
+                               data.cfrc_ext[:14].flat])
+
+    def step(self, a):
+        """Step the simulator forward in time."""
+        pos_before = mass_center(self.model, self.sim)
+        self.do_simulation(a, self.frame_skip)
+        self.set_to_goal()
+        pos_after = mass_center(self.model, self.sim)
+        alive_bonus = 5.0
+        data = self.sim.data
+        lin_vel_cost = 1.25 * (pos_after - pos_before) / self.dt
+        quad_ctrl_cost = 0.1 * np.square(data.ctrl).sum()
+        quad_impact_cost = .5e-6 * np.square(data.cfrc_ext).sum()
+        quad_impact_cost = min(quad_impact_cost, 10)
+        r = lin_vel_cost - quad_ctrl_cost - quad_impact_cost + alive_bonus
+        d = bool((data.qpos[2] < 0.4) or (data.qpos[2] > 2.0))
+        return self._get_obs(), np.nan_to_num(r), d, dict(
+            reward_linvel=lin_vel_cost,
+            reward_quadctrl=-quad_ctrl_cost,
+            reward_alive=alive_bonus,
+            reward_impact=-quad_impact_cost)
+
+    def reset_model(self):
+        """Reset the humanoid to a starting location."""
+        c = 0.01
+        qpos = self.np_random.uniform(low=-c, high=c, size=self.model.nq)
+        qvel = self.np_random.uniform(low=-c, high=c, size=self.model.nv)
+        qvel[0] = 2.0  # set the agent to have a forward momentum
+        self.set_state(self.init_qpos + qpos, self.init_qvel + qvel)
+        return self._get_obs()
+
+    def viewer_setup(self):
+        """Create the viewer."""
+        self.update_viewer()
+
+    def update_viewer(self):
+        """Update the camera position and orientation."""
+        if self.viewer is not None:
+            x, y = self.get_xy()
+            self.viewer.cam.azimuth = 0
+            self.viewer.cam.distance = 35.
+            self.viewer.cam.elevation = -90
+            self.viewer.cam.lookat[0] = 4.
+            self.viewer.cam.lookat[1] = 4.
+
+    def get_ori(self):
+        """Return the orientation of the agent."""
+        rot = self.sim.data.qpos[3:7]
+        ori = q_mult(q_mult(rot, [0, 1, 0, 0]), q_inv(rot))[1:3]
+        return math.atan2(ori[1], ori[0])
+
+    def set_xy(self, xy):
+        """Set the x,y position of the agent."""
+        qpos = np.copy(self.sim.data.qpos)
+        qpos[0] = xy[0]
+        qpos[1] = xy[1]
+        self.set_state(qpos, self.sim.data.qvel)
+
+    def set_goal(self, goal):
+        """Set the goal position of the agent."""
+        self._goal = goal
+
+    def set_to_goal(self):
+        """Update the goal position of the goal agent."""
+        qpos = np.copy(self.sim.data.qpos)
+        qvel = np.copy(self.sim.data.qvel)
+        if self._goal is not None:
+            qpos[24:24 + self._goal.size] = self._goal
+        qpos[26] = 2.4
+        qvel[23:46] = 0.0
+        self.set_state(qpos, qvel)
+
+    def get_xy(self):
+        """Return the x,y position of the agent."""
+        return self.sim.data.qpos[:2]
