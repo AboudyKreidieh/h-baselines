bd9ae840b06e3a345e59d51cd55ea28b2a0d4513
AboudyKreidieh <akreidieh@gmail.com>
2020-09-12 21:29:35 -0700

    updated environments to match model-v3

--- hbaselines/envs/mixed_autonomy/params/highway.py
+++ hbaselines/envs/mixed_autonomy/params/highway.py
@@ -114,10 +114,15 @@ def get_flow_params(fixed_boundary,
         acceleration_controller=(IDMController, {
             "a": 1.3,
             "b": 2.0,
-            "noise": 0.3 if INCLUDE_NOISE else 0.0
+            "noise": 0.3 if INCLUDE_NOISE else 0.0,
+            "display_warnings": False,
+            "fail_safe": [
+                'obey_speed_limit', 'safe_velocity', 'feasible_accel'],
         }),
         car_following_params=SumoCarFollowingParams(
-            min_gap=0.5
+            min_gap=0.5,
+            # right of way at intersections + obey limits on deceleration
+            speed_mode=12
         ),
         lane_change_params=SumoLaneChangeParams(
             model="SL2015",
@@ -139,6 +144,14 @@ def get_flow_params(fixed_boundary,
         "rl",
         num_vehicles=0,
         acceleration_controller=(RLController, {}),
+        car_following_params=SumoCarFollowingParams(
+            min_gap=0.5,
+            # right of way at intersections + obey limits on deceleration
+            speed_mode=12,
+        ),
+        lane_change_params=SumoLaneChangeParams(
+            lane_change_mode=0,  # no lane changes
+        ),
     )
 
     inflows.add(
@@ -182,6 +195,7 @@ def get_flow_params(fixed_boundary,
             horizon=HORIZON,
             warmup_steps=warmup_steps,
             sims_per_step=3,
+            done_at_exit=False,
             additional_params={
                 "max_accel": 0.5,
                 "max_decel": 0.5,
@@ -190,7 +204,7 @@ def get_flow_params(fixed_boundary,
                 "acceleration_penalty": acceleration_penalty,
                 "inflows": None if fixed_boundary else INFLOWS,
                 "rl_penetration": PENETRATION_RATE,
-                "num_rl": 10,
+                "num_rl": float("inf") if multiagent else 10,
                 "control_range": [500, 2300],
                 "expert_model": (IDMController, {
                     "a": 1.3,
