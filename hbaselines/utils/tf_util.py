0537b6736f0eb8d44f4ecf219a7c0444f254002b
AboudyKreidieh <akreidieh@gmail.com>
2020-10-12 11:38:46 -0700

    removed on-policy object

--- hbaselines/utils/tf_util.py
+++ hbaselines/utils/tf_util.py
@@ -519,3 +519,179 @@ def create_conv(obs,
                 pi_h = tf.concat([pi_h, pi_h_ignored], 1)
 
         return pi_h
+
+
+def gae_returns(mb_rewards, mb_values, mb_dones, last_values, gamma, lam):
+    """Compute the bootstrapped/discounted returns.
+
+    Parameters
+    ----------
+    mb_rewards : array_like
+        a minibatch of rewards from a given environment
+    mb_values : array_like
+        a minibatch of values computed by the policy from a given
+        environment
+    mb_dones : array_like
+        a minibatch of done masks from a given environment
+    last_values : array_like
+        the value associated with the current observation within the
+        environment
+    gamma : float
+        discount factor
+    lam : float
+        factor for trade-off of bias vs variance for Generalized Advantage
+        Estimator
+
+    Returns
+    -------
+    array_like
+        GAE-style expected discounted returns.
+    """
+    n_steps = mb_rewards.shape[0]
+
+    # Discount/bootstrap off value fn.
+    mb_advs = np.zeros_like(mb_rewards)
+    mb_vactual = np.zeros_like(mb_rewards)
+    lastgaelam = 0
+    for t in reversed(range(n_steps)):
+        if t == n_steps - 1:
+            nextnonterminal = 1.0 - mb_dones[-1]
+            nextvalues = last_values
+            mb_vactual[t] = mb_rewards[t]
+        else:
+            nextnonterminal = 1.0 - mb_dones[t+1]
+            nextvalues = mb_values[t+1]
+            mb_vactual[t] = mb_rewards[t] \
+                + gamma * nextnonterminal * nextvalues
+        delta = mb_rewards[t] \
+            + gamma * nextvalues * nextnonterminal - mb_values[t]
+        mb_advs[t] = lastgaelam = delta \
+            + gamma * lam * nextnonterminal * lastgaelam
+    mb_returns = mb_advs + mb_values
+
+    return mb_returns
+
+
+def process_minibatch(mb_obs,
+                      mb_contexts,
+                      mb_actions,
+                      mb_values,
+                      mb_neglogpacs,
+                      mb_all_obs,
+                      mb_rewards,
+                      mb_returns,
+                      mb_dones,
+                      last_values,
+                      gamma,
+                      lam,
+                      num_envs):
+    """Process a minibatch of samples.
+
+    This method re-formats the data to numpy arrays that can be passed to
+    the tensorflow placeholders, and computes the GAE terms
+
+    Parameters
+    ----------
+    mb_obs : array_like
+        a minibatch of observations
+    mb_contexts : array_like
+        a minibatch of contextual terms
+    mb_actions : array_like
+        a minibatch of actions
+    mb_values : array_like
+        a minibatch of estimated values by the policy
+    mb_neglogpacs : array_like
+        a minibatch of the negative log-likelihood of performed actions
+    mb_all_obs : array_like
+        a minibatch of full state observations (for multiagent envs)
+    mb_rewards : array_like
+        a minibatch of environment rewards
+    mb_returns : array_like
+        a minibatch of expected discounted returns
+    mb_dones : array_like
+        a minibatch of done masks
+    last_values : array_like
+        the value associated with the current observation within the
+        environment
+    gamma : float
+        discount factor
+    lam : float
+        factor for trade-off of bias vs variance for Generalized Advantage
+        Estimator
+    num_envs : int
+        number of environments used to run simulations in parallel.
+
+    Returns
+    -------
+    array_like
+        the reformatted minibatch of observations
+    array_like
+        the reformatted minibatch of contextual terms
+    array_like
+        the reformatted minibatch of actions
+    array_like
+        the reformatted minibatch of estimated values by the policy
+    array_like
+        the reformatted minibatch of the negative log-likelihood of
+        performed actions
+    array_like
+        the reformatted minibatch of full state observations (for
+        multiagent envs)
+    array_like
+        the reformatted minibatch of environment rewards
+    array_like
+        the reformatted minibatch of expected discounted returns
+    array_like
+        the reformatted minibatch of done masks
+    array_like
+        a minibatch of estimated advantages
+    int
+        the number of sampled steps in the minibatch
+    """
+    n_steps = 0
+
+    for env_num in range(num_envs):
+        # Convert the data to numpy arrays.
+        mb_obs[env_num] = np.concatenate(mb_obs[env_num], axis=0)
+        mb_rewards[env_num] = np.asarray(mb_rewards[env_num])
+        mb_actions[env_num] = np.concatenate(mb_actions[env_num], axis=0)
+        mb_values[env_num] = np.concatenate(mb_values[env_num], axis=0)
+        mb_neglogpacs[env_num] = np.concatenate(
+            mb_neglogpacs[env_num], axis=0)
+        mb_dones[env_num] = np.asarray(mb_dones[env_num])
+        n_steps += mb_obs[env_num].shape[0]
+
+        # Compute the bootstrapped/discounted returns.
+        mb_returns[env_num] = gae_returns(
+            mb_rewards=mb_rewards[env_num],
+            mb_values=mb_values[env_num],
+            mb_dones=mb_dones[env_num],
+            last_values=last_values[env_num],
+            gamma=gamma,
+            lam=lam,
+        )
+
+    # Concatenate the stored data.
+    if num_envs > 1:
+        mb_obs = np.concatenate(mb_obs, axis=0)
+        mb_contexts = np.concatenate(mb_contexts, axis=0)
+        mb_actions = np.concatenate(mb_actions, axis=0)
+        mb_values = np.concatenate(mb_values, axis=0)
+        mb_neglogpacs = np.concatenate(mb_neglogpacs, axis=0)
+        mb_all_obs = np.concatenate(mb_all_obs, axis=0)
+        mb_returns = np.concatenate(mb_returns, axis=0)
+    else:
+        mb_obs = mb_obs[0]
+        mb_contexts = mb_contexts[0]
+        mb_actions = mb_actions[0]
+        mb_values = mb_values[0]
+        mb_neglogpacs = mb_neglogpacs[0]
+        mb_all_obs = mb_all_obs[0]
+        mb_returns = mb_returns[0]
+
+    # Compute the advantages.
+    advs = mb_returns - mb_values
+    mb_advs = (advs - advs.mean()) / (advs.std() + 1e-8)
+
+    return mb_obs, mb_contexts, mb_actions, mb_values, mb_neglogpacs, \
+        mb_all_obs, mb_rewards, mb_returns, mb_dones, mb_advs, n_steps
