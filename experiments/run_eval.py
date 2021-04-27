ee115d15ec2231189026292774aa64256176343c
AboudyKreidieh <akreidieh@gmail.com>
2021-04-26 23:16:17 -0700

    minor

--- experiments/run_eval.py
+++ experiments/run_eval.py
@@ -45,7 +45,8 @@ def main(args):
     if flags.env_name is not None:
         env_name = flags.env_name
         # Override n_agents
-        hp["policy_kwargs"]["n_agents"] = 100
+        if "n_agents" in hp["policy_kwargs"]:
+            hp["policy_kwargs"]["n_agents"] = 100
     hp['num_envs'] = 1
     hp['render_eval'] = not flags.no_render  # to visualize the policy
     multiagent = env_name.startswith("multiagent")
